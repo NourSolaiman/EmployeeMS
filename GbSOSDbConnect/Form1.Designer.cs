fc9c845bf80ffa06121687d6225e536757797b5c
Marcus <marcus.henriksson@restory.ai>
2023-01-18 11:22:24 +0100

    Insert finished

--- GbSOSDbConnect/Form1.Designer.cs
+++ GbSOSDbConnect/Form1.Designer.cs
@@ -28,20 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(87, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(226, 30);
+            this.txtName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Age:";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAge.Location = new System.Drawing.Point(87, 64);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(226, 30);
+            this.txtAge.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(87, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 70);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "INSERT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(994, 598);
+            this.ClientSize = new System.Drawing.Size(999, 667);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- GbSOSDbConnect/Form1.cs
+++ GbSOSDbConnect/Form1.cs
@@ -30,5 +30,42 @@ namespace GbSOSDbConnect
 
             conn = new MySqlConnection(connString);
         }
+
+        private void InsertPersonToDB()
+        {
+            //Hämta värden från textfält
+            string name = txtName.Text;
+            int age = Convert.ToInt32( txtAge.Text);
+
+            //Bygg upp SQL querry
+            string sqlQuerry = $"CALL insertPeople('{name}', {age});";
+
+            //Skapar ett MySqlCOmmand objekt
+            MySqlCommand cmd = new MySqlCommand(sqlQuerry, conn);
+
+            //Skapa ett TryCatch Block
+            try
+            {
+                //Öppna Connection
+                conn.Open();
+
+                //Exekvera kommando
+                cmd.ExecuteReader();
+
+                //Stänga Connection
+                conn.Close();
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            //Bekräftelse till användare
+            MessageBox.Show("Insert Finished Successfully!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InsertPersonToDB();
+        }
     }
 }
