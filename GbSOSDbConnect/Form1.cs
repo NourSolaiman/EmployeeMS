1513378898e76539b897d59034c680a875ab3a0a
Marcus <marcus.henriksson@restory.ai>
2023-01-18 10:07:57 +0100

    Init och skapat MySQLConnection

--- GbSOSDbConnect/Form1.cs
+++ GbSOSDbConnect/Form1.cs
@@ -13,10 +13,22 @@ namespace GbSOSDbConnect
 {
     public partial class Form1 : Form
     {
+        // Skapa ref för MySQLConnection objekt
+        MySqlConnection conn;
 
         public Form1()
         {
             InitializeComponent();
+
+            //Bygg upp MySQLCOnneection obejkt
+            string server = "localhost";
+            string database = "gbsosdbapplikation";
+            string user = "root";
+            string password = "SokrateS13";
+
+            string connString = $"SERVER={server};DATABASE={database};UID={user};PASSWORD={password};";
+
+            conn = new MySqlConnection(connString);
         }
     }
 }
